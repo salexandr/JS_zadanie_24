70bf2c3d1f7ba794991447ee19f46b5b5e50d7d5
Alexandr <a.senia@yandex.ru>
2016-10-30 19:13:18 +0400

    variant2

---
+++ variant2/js/view.js
@@ -0,0 +1,33 @@
+﻿define(
+    'view',
+	['model', 'jquery'],
+    function() {
+
+function View(model){
+	var self = this;
+	
+	function init(){
+		var wrapper = tmpl($('#wrapper-template').html());
+		
+	    $('body').append(wrapper);	
+		self.elements = {
+			input: $('.item-value'),
+			addBtn: $('.item-add'),
+			listContainer: $('.item-list'),
+			itemEdit: $('.item-list')
+		};
+		self.renderList(model.data);
+	};
+	
+	self.renderList = function (data){
+		var list = tmpl($('#list-template').html(), {data: data});
+		self.elements.listContainer.html(list);
+	};
+	
+	init();
+}
+        
+        return View;
+
+    }
+);
